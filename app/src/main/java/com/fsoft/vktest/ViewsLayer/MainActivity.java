4d4ffb56ffe442b15e637341bd3eb04992d25a7f
drfailov <fsoft-p@narod.ru>
2018-05-07 22:17:25 +0300

    07.05 Решена проблема сборки, теперь проект собрался, работает боковое слайд-меню

--- app/src/main/java/com/fsoft/vktest/ViewsLayer/MainActivity.java
+++ app/src/main/java/com/fsoft/vktest/ViewsLayer/MainActivity.java
@@ -1,8 +1,10 @@
 package com.fsoft.vktest.ViewsLayer;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
+import android.view.Window;
 
 import com.fsoft.vktest.R;
 
@@ -10,13 +12,14 @@ import com.fsoft.vktest.R;
  * Created by Dr. Failov on 28.11.2017.
  */
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends Activity {
     private Handler handler = new Handler();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         //проверить запущен ли сервис. если нет - запустить.
         setContentView(R.layout.activity_main);
     }
